7bf40c5305b186381a7e9b0b011e330ea0f4710a
mkusf <107431102+mkusf@users.noreply.github.com>
2025-10-04 23:30:13 -0700

    Display vars in exec_Test function

--- test/src/exec_order_tester.cpp
+++ test/src/exec_order_tester.cpp
@@ -7,14 +7,14 @@
 #include "parser.h"
 #include <iostream>
 
-int exec_Test(const std::vector<Node> &result, const std::string &output_Name, long total_Memory);
+int exec_Test(const std::vector<Node> &result, const std::string &output_Name, long total_Memory, int total_Nodes);
 int main() {
     const std::string sm_Test_1 = "test/test_cases/example1.txt";
     ParseResult sm_Test_Data = parse_file(sm_Test_1);
     
     std::vector<Node> small_Test_1_Solution = executeOrder(sm_Test_Data.nodes,/*outputName*/,sm_Test_Data.max_memory);
 
-    printf("Total time small_test_1: %d", exec_Test(small_Test_1_Solution, /* outputName*/, sm_Test_Data.max_memory));
+    printf("Total time small_test_1: %d", exec_Test(small_Test_1_Solution, /* outputName*/, sm_Test_Data.max_memory, sm_Test_Data.nodes.size()));
 }
 /**
  *  This function will track current memory usage, current executing node, and total execution time.
@@ -28,10 +28,13 @@ int main() {
  *  returns:
  *  int total_time - The time it takes to execute the DAG
  */
-int exec_Test(const std::vector<Node> &result, const std::string &output_name, long total_memory) {
-    int total_time = 0;
+int exec_Test(const std::vector<Node> &result, const std::string &output_name, long total_memory, int total_Nodes) {
+    int total_Time = 0;
+    int current_Mem_Usage = 0;
+    int *current_Mem_nodes[total_Nodes];
     /*
     I get the address of vector, i need to iterate through 
     */
+
     return total_time;
 }
