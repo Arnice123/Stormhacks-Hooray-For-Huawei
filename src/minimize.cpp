179933483b74f72d9ef8ecbad4858bfc95f6f343
Martin Anderson <mander24@students.cs.ubc.ca>
2025-10-05 02:07:26 -0700

    Set up reverse iterator and local variables for maxMemorySpike function

--- src/minimize.cpp
+++ src/minimize.cpp
@@ -1,19 +1,26 @@
 #include "minimize.h"
 
+#include "map"
+
 std::vector<Node> MinimizeRecomputation::ExecuteOrder(const std::vector<Node> &all_nodes, const std::string &output_name,
-                               long total_memory) {
+                                                      long total_memory) {
     // Use unused parameter
     (void) output_name;
 
     std::vector<Node> nodes = all_nodes;
     long memoryUsage = MinimizeRecomputation::maxMemorySpike(nodes);
     while (memoryUsage > total_memory) {
-        
     }
-    
+
     return nodes;
 }
 
 long MinimizeRecomputation::maxMemorySpike(const std::vector<Node> &nodes) {
-    return 0;
+    long max = 0;
+    long curr = 0;
+    std::map<std::string, bool> nodesInMemory;
+    for (std::vector<Node>::reverse_iterator riter = nodes.rbegin(); riter != nodes.rend(); ++riter) {
+        // do something
+    }
+    return max;
 }
